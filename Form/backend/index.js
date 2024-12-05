1c75cf19fe20b65e4a0361966e5eedb801a49321
uditchauhan3 <uditchauhan72@gmail.com>
2024-12-05 16:43:59 +0530

    bla bla

--- Form/backend/index.js
+++ Form/backend/index.js
@@ -6,18 +6,18 @@ const cors = require('cors');
 
 const formRoutes = require('./routes/form');
 const responseRoutes = require('./routes/response');
-const bodyParser = require('body-parser');
-const bodyParser = require('body-parser');
+
 
 const app = express();
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors({
-  origin: 'https://form-builder-oxa5.vercel.app/', // Your frontend URL
+  origin: 'https://form-builder-oxa5.vercel.app', // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
 
-app.use(bodyParser.json());
+
 
 // Routes
 app.use('/api/forms', formRoutes);
