087ff712a8fa5fb257f934a893811cb4647cc3d3
uditchauhan3 <131523304+uditchauhan3@users.noreply.github.com>
2024-12-05 16:14:25 +0530

    Update App.jsx

--- Form/frontend/src/App.jsx
+++ Form/frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import FormEditor from './Components/FormEditor';
 
-useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await api.get('https://form-builder-iota-five.vercel.app/'); // Replace `/endpoint` with your API path
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+//useEffect(() => {
+  //  const fetchData = async () => {
+    //  try {
+    //    const response = await api.get('https://form-builder-iota-five.vercel.app/'); // Replace `/endpoint` with your API path
+     //   setData(response.data);
+     // } catch (error) {
+     //   console.error('Error fetching data:', error);
+     // }
+   // };
 
-    fetchData();
-  }, []);
+    //fetchData();
+  //}, []);
 
 const App = () => (
   <div className="bg-gradient-to-r from-blue-100 via-purple-100 to-pink-100 min-h-screen flex items-center justify-center">
