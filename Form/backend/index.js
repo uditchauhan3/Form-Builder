74c730001bdb79524cd5b40749ef5dfc199cb3ca
uditchauhan3 <UDITCHAUHAN.21011565@gehu.ac.in>
2024-12-05 18:06:20 +0530

    Updated: CORS

--- Form/backend/index.js
+++ Form/backend/index.js
@@ -31,7 +31,7 @@ app.use('/', responseRoutes);
 app.options('*', cors()); // Handle preflight requests
 
 // MongoDB connection
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
