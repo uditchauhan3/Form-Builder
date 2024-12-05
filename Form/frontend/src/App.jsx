fb194624eb302c8193f9e0680067dd3877d56b47
uditchauhan3 <uditchauhan72@gmail.com>
2024-12-05 17:08:02 +0530

    api update

--- Form/frontend/src/App.jsx
+++ Form/frontend/src/App.jsx
@@ -1,40 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import FormEditor from './Components/FormEditor';
 import axios from 'axios';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // const [data, setData] = useState(null);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Replace with your actual backend API endpoint
-        const response = await axios.get('https://form-builder-iota-five.vercel.app/api/forms', {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          withCredentials: true // If you're using credentials
-        });
-        setData(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setError(error);
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       // Replace with your actual backend API endpoint
+  //       const response = await axios.get('https://form-builder-iota-five.vercel.app/api/forms', {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //         withCredentials: true // If you're using credentials
+  //       });
+  //       setData(response.data);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //       setError(error);
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error loading data: {error.message}</div>;
+  // if (loading) return <div>Loading...</div>;
+  // if (error) return <div>Error loading data: {error.message}</div>;
 
   return (
     <div className="bg-gradient-to-r from-blue-100 via-purple-100 to-pink-100 min-h-screen flex items-center justify-center">
-      <FormEditor data={data} />
+      <FormEditor />
     </div>
   );
 };
