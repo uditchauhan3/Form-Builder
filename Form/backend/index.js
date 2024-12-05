99bd394e825c3ea4c0f82d480426004445456337
uditchauhan3 <131523304+uditchauhan3@users.noreply.github.com>
2024-12-05 16:20:23 +0530

    Update index.js

--- Form/backend/index.js
+++ Form/backend/index.js
@@ -17,9 +17,10 @@ app.use(cors({
 // Routes
 app.use('/api/forms', formRoutes);
 app.use('/api/responses', responseRoutes);
-
+app.options('*',cors());
 // MongoDB connection
 const PORT = process.env.PORT || 5001;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-  .catch((error) => console.log(error));
+mongoose.connect(process.env.MONGO_URI);
+app.listen(PORT, ()=>{
+  console.log(`server running on ${PORT}`)
+});
